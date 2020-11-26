28ae5b7c2aa763436f612e7d064a4bc771ffb3e8
Andy <mingandyliao@gmail.com>
2020-11-25 19:48:34 -0500

    Barebones app with api service GETing configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { studentApiService } from "../app/service/studentapi.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'barebones-angular';
+  studentsList: any;
+
+  constructor(private _studentApiService: studentApiService) {}
+
+  //calling api get service
+  getData() {
+    this._studentApiService.getData().subscribe(
+      (response) => {
+        this.studentsList = response;
+        console.log(this.studentsList);
+      },
+      (error) => {
+        console.error("Error retrieving student properties ", error);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
 }
